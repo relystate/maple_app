a8978a2a92688c1c67cfaed649a747afca15a5e5
relystate <relystate@gmail.com>
2012-11-07 06:16:51 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MapleApp::Application.config.secret_token = '53108b0007543f7546fe50425d45497ae92639dba80c5d9d87bbeb15d79a64e566cbe2202f5d7008955330b2c4e00b5504d19569e335e9952af0a236200611e6'
